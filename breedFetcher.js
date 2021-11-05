e461296846df00ff16b1d918e429d078c1e1159b
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-05 00:10:13 +0000

    rename file from breedERFetcher to breed

---
+++ breedFetcher.js
@@ -0,0 +1,19 @@
+const request = require('request');
+// const argv = process.argv.slice(2);
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, respond, body) {
+    if (error) {
+    callback(error, null);//if website doesn't work return error
+    }
+  
+    const data = JSON.parse(body);//turn body into object
+    if (data[0] === undefined) {
+      callback(null,`${breedName} is not found`);//if name of cat is not available
+    } else {
+      callback(null,data[0]["description"]);//show description of cat in the database
+    }
+  });
+};
+module.exports = {fetchBreedDescription};
--- breederFetcher.js
+++ breederFetcher.js
@@ -1,17 +1,19 @@
 const request = require('request');
 const argv = process.argv.slice(2);
 
+
+const fetchBreedDescription = function(breedName, callback) {
 request(`https://api.thecatapi.com/v1/breeds/search?q=${argv[0]}`, function(error, respond, body) {
   const data = JSON.parse(body);//turn body into object
   
   if (error) {
-    return console.log(`Request not found`,error);//if website doesn't work return error
+    callback(`Request not found`,error);//if website doesn't work return error
   }
   
   if (data[0] === undefined) {
-    console.log(`${argv} is not found`);//if name of cat is not available
+    callback(`${argv} is not found`);//if name of cat is not available
   } else {
-    console.log(data[0]["description"]);//show description of cat in the database
+    callback(data[0]["description"]);//show description of cat in the database
   }
 });
-
+};
